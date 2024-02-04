f743a49e7e48c87dd20450b6e63e9b6d683e24de
Bruno Lazzara Queiroz <bruno.queiroz@acpgroup.com.br>
2024-02-04 12:38:24 -0300

    Create the Coupon API project with initial files

---
+++ Orange.Services.CouponAPI/Data/OrangeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orange.Services.CouponAPI.Models.Entity;
+
+namespace Orange.Services.CouponAPI.Data
+{
+    public class OrangeDbContext : DbContext
+    {
+        public OrangeDbContext(DbContextOptions<OrangeDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+    }
+}
---
+++ Orange.Services.CouponAPI/Models/Entity/Coupon.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orange.Services.CouponAPI.Models.Entity
+{
+    public class Coupon
+    {
+        [Key]
+        public int CouponId { get; set; }
+        [Required]
+        public string CouponCode { get; set; }
+        [Required]
+        public double DiscountAmount { get; set; }
+        public int MinAmount { get; set; }
+    }
+}
