73a84b0879742806545f702d846aa4c4b78a107f
Bruno Lazzara Queiroz <bruno.queiroz@acpgroup.com.br>
2024-02-05 00:27:39 -0300

    Modify ResponseDTO so the result can be of any type, not just class type

--- Orange.Services.CouponAPI/Models/DTO/ResponseDTO.cs
+++ Orange.Services.CouponAPI/Models/DTO/ResponseDTO.cs
@@ -1,6 +1,6 @@
 ﻿namespace Orange.Services.CouponAPI.Models.DTO
 {
-    public class ResponseDTO<TResult> where TResult : class
+    public class ResponseDTO<TResult>
     {
         public TResult? Result { get; set; }
         public string Message { get; set; } = string.Empty;
