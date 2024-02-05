5efa1d826e9c3f186e5eb1c1e7b7c83b2fe87fd4
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2024-02-05 16:05:46 -0300

    Create view that shows a list of all coupons

---
+++ Orange.Web/Controllers/CouponController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orange.Models.DTO;
+using Orange.Web.Services.Interfaces;
+
+namespace Orange.Web.Controllers
+{
+    public class CouponController : Controller
+    {
+        private readonly ICouponService _couponService;
+        public CouponController(ICouponService couponService)
+        {
+            _couponService = couponService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<CouponDTO> coupons = await _couponService.GetAllAsync();
+            return View(coupons);
+        }
+    }
+}
