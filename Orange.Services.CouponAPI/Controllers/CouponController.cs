57fc26531c31684442b1c996c4bfad9d6c94bb31
Bruno Lazzara Queiroz <bruno.queiroz@acpgroup.com.br>
2024-02-05 00:30:25 -0300

    Add and implement all the remaining CRUD endpoints for Coupons, including get coupon by code

--- Orange.Services.CouponAPI/Controllers/CouponController.cs
+++ Orange.Services.CouponAPI/Controllers/CouponController.cs
@@ -63,5 +63,98 @@ namespace Orange.Services.CouponAPI.Controllers
                 return BadRequest();
             }
         }
+
+        [HttpGet("{code}")]
+        public ActionResult Get(string code)
+        {
+            try
+            {
+                var response = new ResponseDTO<CouponDTO>();
+
+                var coupon = _context.Coupons.FirstOrDefault(c => c.CouponCode.ToLower() == code.ToLower());
+                response.Result = _mapper.Map<CouponDTO>(coupon);
+
+                if (response.Result == null)
+                {
+                    response.Message = "Coupon not found.";
+                }
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromBody] CouponDTO couponDTO)
+        {
+            try
+            {
+                var response = new ResponseDTO<CouponDTO>();
+
+                Coupon coupon = _mapper.Map<Coupon>(couponDTO);
+                _context.Coupons.Add(coupon);
+                _context.SaveChanges();
+
+                response.Result = _mapper.Map<CouponDTO>(coupon);
+
+                return StatusCode(201, response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpPut]
+        public ActionResult Put([FromBody] CouponDTO couponDTO)
+        {
+            try
+            {
+                var response = new ResponseDTO<CouponDTO>();
+
+                Coupon coupon = _mapper.Map<Coupon>(couponDTO);
+                _context.Coupons.Update(coupon);
+                _context.SaveChanges();
+
+                response.Result = _mapper.Map<CouponDTO>(coupon);
+
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public ActionResult Delete(int id)
+        {
+            try
+            {
+                var response = new ResponseDTO<bool>();
+
+                Coupon? coupon = _context.Coupons.FirstOrDefault(c => c.CouponId == id);
+                if (coupon == null)
+                {
+                    response.Message = "Coupon not found.";
+                    response.Result = false;
+                    return Ok(response);
+                }
+
+                _context.Coupons.Remove(coupon);
+                _context.SaveChanges();
+
+                response.Message = "Coupon deleted.";
+                response.Result = true;
+                return Ok(response);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+        }
     }
 }
