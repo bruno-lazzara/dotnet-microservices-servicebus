8bdf923342396804b933852ce42c9a3cf1c58696
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2024-02-05 01:29:31 -0300

    Add RequestDTO

---
+++ Orange.Models/DTO/RequestDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Orange.Models.DTO
+{
+    public class RequestDTO<TRequest>
+    {
+        public HttpMethod HttpMethod { get; set; } = HttpMethod.Get;
+        public string Url { get; set; }
+        public TRequest Data { get; set; }
+        public string AccessToken { get; set; }
+    }
+}
