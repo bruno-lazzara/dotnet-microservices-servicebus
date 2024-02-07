65d84cf9bc853c05dba1bf3b221a588e015dc6ec
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2024-02-06 23:41:26 -0300

    Add nav menu buttons and controller actions for login and register

---
+++ Orange.Web/Controllers/AuthController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Orange.Models.DTO.Auth;
+using Orange.Web.Services.Interfaces;
+
+namespace Orange.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+    }
+}
