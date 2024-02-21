6ad0d6c5b16fd1f3f3e5a8969a1c3554771b7f2e
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2024-02-21 19:16:33 -0300

    Add annotations to the UserDTOs

--- Orange.Models/DTO/Auth/LoginUserDTO.cs
+++ Orange.Models/DTO/Auth/LoginUserDTO.cs
@@ -1,8 +1,13 @@
-﻿namespace Orange.Models.DTO.Auth
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orange.Models.DTO.Auth
 {
     public class LoginUserDTO
     {
+        [Required]
         public string UserName { get; set; }
+
+        [Required]
         public string Password { get; set; }
     }
 }
--- Orange.Models/DTO/Auth/RegisterUserDTO.cs
+++ Orange.Models/DTO/Auth/RegisterUserDTO.cs
@@ -1,8 +1,21 @@
-﻿namespace Orange.Models.DTO.Auth
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Orange.Models.DTO.Auth
 {
-    public class RegisterUserDTO : BaseUserDTO
+    public class RegisterUserDTO
     {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
         public string Password { get; set; }
+
         public string Role { get; set; }
     }
 }
