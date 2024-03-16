c410b23e19659dbf41437a70c6e9aa2df7f4b03c
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2024-03-16 13:19:02 -0300

    Allow to create, edit and delete products from the web project

--- Orange.Web/Controllers/ProductController.cs
+++ Orange.Web/Controllers/ProductController.cs
@@ -42,6 +42,36 @@ namespace Orange.Web.Controllers
             return RedirectToAction("Index");
         }
 
+        public async Task<IActionResult> Edit(int id)
+        {
+            var product = await _productService.GetProductByIdAsync(id);
+            if (product == null)
+            {
+                TempData["error"] = "Product not found";
+                return RedirectToAction("Index");
+            }
+            return View(product);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(ProductDTO product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(product);
+            }
+
+            var productResponse = await _productService.UpdateProductAsync(product);
+            if (productResponse == null)
+            {
+                TempData["error"] = "Error on editing product";
+                return View(product);
+            }
+
+            TempData["success"] = "Product edited successfully";
+            return RedirectToAction("Index");
+        }
+
         public async Task<IActionResult> Delete(int id)
         {
             var deleted = await _productService.DeleteProductAsync(id);
