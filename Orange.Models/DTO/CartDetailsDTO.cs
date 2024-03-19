d6524d19566bf15b95e7cffca35f1f2bf3481a92
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2024-03-18 21:06:23 -0300

    Create initial shopping cart API project

---
+++ Orange.Models/DTO/CartDetailsDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Orange.Models.DTO
+{
+    public class CartDetailsDTO
+    {
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+        public CartHeaderDTO? CartHeader { get; set; }
+        public ProductDTO? Product { get; set; }
+    }
+}
---
+++ Orange.Services.ShoppingCartAPI/Models/Entity/CartDetails.cs
@@ -0,0 +1,21 @@
+﻿using Orange.Models.DTO;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Orange.Services.ShoppingCartAPI.Models.Entity
+{
+    public class CartDetails
+    {
+        [Key]
+        public int CartDetailsId { get; set; }
+        public int CartHeaderId { get; set; }
+        public int ProductId { get; set; }
+        public int Count { get; set; }
+
+        [ForeignKey("CartHeaderId")]
+        public CartHeader CartHeader { get; set; }
+
+        [NotMapped]
+        public ProductDTO Product { get; set; }
+    }
+}
