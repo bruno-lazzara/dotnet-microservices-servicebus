a9a1a4d1be3fa71e53a5781c715ec2559fb5b380
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2024-03-20 14:21:14 -0300

    Setup Email API and apply first migration

---
+++ Orange.Services.EmailAPI/Data/OrangeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orange.Services.EmailAPI.Models.Entity;
+
+namespace Orange.Services.EmailAPI.Data
+{
+    public class OrangeDbContext : DbContext
+    {
+        public OrangeDbContext(DbContextOptions<OrangeDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<EmailLogger> EmailLoggers { get; set; }
+    }
+}
