eb105bbe7dcc69a00f15a706974c3d4be877a5b1
Bruno Lazzara Queiroz <lazzara.bruno@gmail.com>
2024-03-26 22:21:39 -0300

    Create initial project for the Reward API and apply first migration

---
+++ Orange.Services.RewardAPI/Data/OrangeDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Orange.Services.RewardAPI.Models.Entity;
+
+namespace Orange.Services.RewardAPI.Data
+{
+    public class OrangeDbContext : DbContext
+    {
+        public OrangeDbContext(DbContextOptions<OrangeDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Rewards> Rewards { get; set; }
+    }
+}
---
+++ Orange.Services.RewardAPI/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.EntityFrameworkCore;
+using Orange.Services.RewardAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<OrangeDbContext>(option =>
+{
+    option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+ApplyMigrations();
+
+app.Run();
+
+
+void ApplyMigrations()
+{
+    using (var scope = app.Services.CreateScope())
+    {
+        var db = scope.ServiceProvider.GetRequiredService<OrangeDbContext>();
+
+        if (db.Database.GetPendingMigrations().Count() > 0)
+        {
+            db.Database.Migrate();
+        }
+    }
+}
